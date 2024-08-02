2699f23958b17f79d69666e58b48c0b9f446f2b4
chris <zqy@thinkerx.com>
2024-08-02 13:44:33 +0800

    chore: 项目app配置内容

--- model_mall/settings.py
+++ model_mall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ptpeli8qt01!n=u#f-27ofb0t0^$i@4gl0$959w=$vey47x-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
+    'admin'
 ]
 
 MIDDLEWARE = [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
