8f09d9c3a701cd2a8b6193ecadcb58768b5fdc17
zhuqy <1132552771@qq.com>
2024-12-02 10:28:49 +0800

    feat: 增加登录注册功能

--- admin_manage/views.py
+++ admin_manage/views.py
@@ -1,3 +1,46 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from utils.sql_utils import fetchone
+
+
 # Create your views here.
+
+def login(request):
+    """
+    管理端登录
+    :param request:
+    :return:
+    """
+    try:
+        params = request.POST
+        username = params["username"]
+        password = params["password"]
+    except (ValueError, TypeError, KeyError):
+        return JsonResponse(status=400, data={})
+
+    sql = "SELECT id, username, avater, role FROM user WHERE username = %s AND password = %s;"
+    user_res = fetchone(sql, params=(username, password))
+    if not user_res:
+        return JsonResponse(status=200, data={"data": {}, "msg": "用户名/密码错误"})
+    if user_res["role"] not in ("admin", "super"):
+        return JsonResponse(status=200, data={"data": {}, "msg": "权限不足"})
+
+    return JsonResponse(status=200, data=user_res)
+
+
+# 模型上传（包含识别，核心）
+def add_model():
+
+    pass
+
+
+# 模型列表获取
+def get_model_list():
+    pass
+
+
+# 模型编辑
+def edit_model():
+    pass
+
--- client/views.py
+++ client/views.py
@@ -1,10 +1,71 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from utils.sql_utils import execute, fetchone
 
 
 # Create your views here.
 def login(request):
-    pass
+    """
+    用户端登录
+    :param request:
+    :return:
+    """
+    try:
+        params = request.POST
+        username = params["username"]
+        password = params["password"]
+    except (ValueError, TypeError, KeyError):
+        return JsonResponse(status=400, data={})
+
+    sql = "SELECT id, username, avater, role FROM `user` WHERE username = %s AND password = %s;"
+    user_res = fetchone(sql, params=(username, password))
+    if not user_res:
+        return JsonResponse(status=200, data={"data": {}, "msg": "用户名/密码错误"})
+
+    return JsonResponse(status=200, data=user_res)
 
 
 def register(request):
-    pass
+    try:
+        params = request.POST
+        username = params["username"]
+        password = params["password"]
+        role = params["role"]
+        if role not in ("user", "merchant"):
+            raise ValueError("用户类型错误")
+    except (ValueError, TypeError, KeyError) as e:
+        return JsonResponse(status=400, data={"data": {}, "msg": e})
+
+    sql = "SELECT 1 FROM `user` WHERE username = %s;"
+    user_res = fetchone(sql, (username,))
+    if user_res:
+        return JsonResponse(status=200, data={"data": {}, "msg": "用户名已存在"})
+
+    sql = "INSERT INTO `user` (username, password) VALUES (%s, %s);"
+    user_id = execute(sql, (username, password))
+
+    data = {
+        "id": user_id,
+        "role": role,
+        "username": username,
+        "avatar": ""  # 初始头像链接
+    }
+
+    return JsonResponse(status=200, data=data)
+
+
+def get_model_list(request):
+    try:
+        params = request.GET
+        merchant_id = int(params["merchant_id"])
+
+        page = int(params.get("page", "1"))
+        limit = int(params.get("page", "20"))
+        page = 1 if page < 1 else page
+        limit = 500 if limit > 500 else limit
+        offset = (page - 1) * limit
+    except (ValueError, TypeError, KeyError) as e:
+        return JsonResponse(status=400, data={"data": {}, "msg": e})
+
+
