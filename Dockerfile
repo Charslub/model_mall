b94e2063b107de073ba82a76955bb14491127c19
zhuqy <1132552771@qq.com>
2024-12-23 15:18:24 +0800

    feat: 增加Dockerfile配置内容

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,24 @@
-#FROM ubuntu:latest
-#LABEL authors="ghost"
-#
-#ENTRYPOINT ["top", "-b"]
+# 基础镜像
+FROM python:3.12
+
+# 设置工作目录
+WORKDIR /home/model_mall
+
+# 将当前目录下的文件复制到镜像中
+COPY . /home/model_mall
+
+# 创建新用户
+RUN useradd -ms /bin/bash model
+
+# 切换到新用户
+USER model
+
+# 确保 ~/.local/bin 在 PATH 中
+ENV PATH="/home/model/.local/bin:${PATH}"
+
+# 安装项目依赖
+COPY requirements.txt /home/model_mall
+RUN pip install --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+
+# 设置默认命令
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- client/urls.py
+++ client/urls.py
@@ -1,16 +1,16 @@
-from django.template.defaulttags import url
+from django.urls import path
 
 from client.views import user_views, model_views, cart_views
 from client.views.cart_views import CartView
 
 urlpatterns = [
     # 用户接口
-    url(r'^client/login/$', user_views.login, name='client_login'),
-    url(r'^client/register/$', user_views.register, name='client_register'),
+    path(r'client/login/', user_views.login, name='client_login'),
+    path(r'client/register/', user_views.register, name='client_register'),
 
     # 模型接口
-    url(r'^models/$', model_views.get_model_list, name='get_model_list'),
+    path(r'models/', model_views.get_model_list, name='get_model_list'),
 
     # 购物车相关接口
-    url(r'^carts/$', CartView.as_view(), name='cart_operations'),
+    path(r'carts/', CartView.as_view(), name='cart_operations'),
 ]
